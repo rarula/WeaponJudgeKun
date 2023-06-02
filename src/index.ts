5d9c5b10dee719c6d23f528cc9029bf4d3ccd10e
rarula <74240663+rarula@users.noreply.github.com>
2023-06-02 12:59:39 +0900

    ✨ 24時間起動できるように

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import 'dotenv/config';
+import express, { Request, Response } from 'express';
 
 import { setupBot } from './bot';
 
+// バージョン情報
+export const VERSION = 'v4.0.0';
+
 // botを起動
 setupBot();
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({ version: VERSION });
+});
+
+app.listen(3000, () => {
+    console.log('Start on port 3000.');
+});
