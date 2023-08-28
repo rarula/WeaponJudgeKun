a6e8da0c39fba4f14c9dbf3ddc7ffffb5573ee86
rarula <74240663+rarula@users.noreply.github.com>
2023-08-28 11:16:33 +0900

    ♻️ judge関数を適切な場所に移動

--- src/commands/JudgeCommand.ts
+++ src/commands/JudgeCommand.ts
@@ -1,9 +1,10 @@
 import { SlashCommandBuilder } from 'discord.js';
 
-import { judge } from '../judge';
 import { locale } from '../locales';
 import { Command } from '../types/Command';
-import { SpecialWeapon, SubWeapon, WeaponType } from '../types/Weapon';
+import { SpecialWeapon, SubWeapon, Weapon, WeaponType } from '../types/Weapon';
+import { getRandomElem } from '../utils/array';
+import { BLASTERS, BRELLAS, BRUSHES, CHARGERS, DUALIES, ROLLERS, SHOOTERS, SLOSHERS, SPLATANAS, SPLATLINGS, STRINGERS } from '../weapons';
 
 export const JudgeCommand: Command = {
     command: new SlashCommandBuilder()
@@ -22,50 +23,17 @@ export const JudgeCommand: Command = {
                     locale('text-command-judge-des-1'),
                 )
                 .addChoices(
-                    {
-                        name: locale('text-shooter'),
-                        value: 'SHOOTER',
-                    },
-                    {
-                        name: locale('text-splatling'),
-                        value: 'SPLATLING',
-                    },
-                    {
-                        name: locale('text-charger'),
-                        value: 'CHARGER',
-                    },
-                    {
-                        name: locale('text-roller'),
-                        value: 'ROLLER',
-                    },
-                    {
-                        name: locale('text-blaster'),
-                        value: 'BLASTER',
-                    },
-                    {
-                        name: locale('text-slosher'),
-                        value: 'SLOSHER',
-                    },
-                    {
-                        name: locale('text-dualie'),
-                        value: 'DUALIE',
-                    },
-                    {
-                        name: locale('text-brush'),
-                        value: 'BRUSH',
-                    },
-                    {
-                        name: locale('text-stringer'),
-                        value: 'STRINGER',
-                    },
-                    {
-                        name: locale('text-brella'),
-                        value: 'BRELLA',
-                    },
-                    {
-                        name: locale('text-splatana'),
-                        value: 'SPLATANA',
-                    },
+                    { name: locale('text-shooter'), value: 'SHOOTER' },
+                    { name: locale('text-splatling'), value: 'SPLATLING' },
+                    { name: locale('text-charger'), value: 'CHARGER' },
+                    { name: locale('text-roller'), value: 'ROLLER' },
+                    { name: locale('text-blaster'), value: 'BLASTER' },
+                    { name: locale('text-slosher'), value: 'SLOSHER' },
+                    { name: locale('text-dualie'), value: 'DUALIE' },
+                    { name: locale('text-brush'), value: 'BRUSH' },
+                    { name: locale('text-stringer'), value: 'STRINGER' },
+                    { name: locale('text-brella'), value: 'BRELLA' },
+                    { name: locale('text-splatana'), value: 'SPLATANA' },
                 )
         )
         .addStringOption((option) =>
@@ -77,62 +45,20 @@ export const JudgeCommand: Command = {
                     locale('text-command-judge-des-2'),
                 )
                 .addChoices(
-                    {
-                        name: locale('splat-bomb'),
-                        value: 'splat-bomb',
-                    },
-                    {
-                        name: locale('suction-bomb'),
-                        value: 'suction-bomb',
-                    },
-                    {
-                        name: locale('burst-bomb'),
-                        value: 'burst-bomb',
-                    },
-                    {
-                        name: locale('curling-bomb'),
-                        value: 'curling-bomb',
-                    },
-                    {
-                        name: locale('autobomb'),
-                        value: 'autobomb',
-                    },
-                    {
-                        name: locale('ink-mine'),
-                        value: 'ink-mine',
-                    },
-                    {
-                        name: locale('toxic-mist'),
-                        value: 'toxic-mist',
-                    },
-                    {
-                        name: locale('point-sensor'),
-                        value: 'point-sensor',
-                    },
-                    {
-                        name: locale('splash-wall'),
-                        value: 'splash-wall',
-                    },
-                    {
-                        name: locale('sprinkler'),
-                        value: 'sprinkler',
-                    },
-                    {
-                        name: locale('squid-beakon'),
-                        value: 'squid-beakon',
-                    },
-                    {
-                        name: locale('fizzy-bomb'),
-                        value: 'fizzy-bomb',
-                    },
-                    {
-                        name: locale('torpedo'),
-                        value: 'torpedo',
-                    },
-                    {
-                        name: locale('angle-shooter'),
-                        value: 'angle-shooter',
-                    },
+                    { name: locale('splat-bomb'), value: 'splat-bomb' },
+                    { name: locale('suction-bomb'), value: 'suction-bomb' },
+                    { name: locale('burst-bomb'), value: 'burst-bomb' },
+                    { name: locale('curling-bomb'), value: 'curling-bomb' },
+                    { name: locale('autobomb'), value: 'autobomb' },
+                    { name: locale('ink-mine'), value: 'ink-mine' },
+                    { name: locale('toxic-mist'), value: 'toxic-mist' },
+                    { name: locale('point-sensor'), value: 'point-sensor' },
+                    { name: locale('splash-wall'), value: 'splash-wall' },
+                    { name: locale('sprinkler'), value: 'sprinkler' },
+                    { name: locale('squid-beakon'), value: 'squid-beakon' },
+                    { name: locale('fizzy-bomb'), value: 'fizzy-bomb' },
+                    { name: locale('torpedo'), value: 'torpedo' },
+                    { name: locale('angle-shooter'), value: 'angle-shooter' },
                 )
         )
         .addStringOption((option) =>
@@ -144,85 +70,34 @@ export const JudgeCommand: Command = {
                     locale('text-command-judge-des-3'),
                 )
                 .addChoices(
-                    {
-                        name: locale('big-bubbler'),
-                        value: 'big-bubbler',
-                    },
-                    {
-                        name: locale('booyah-bomb'),
-                        value: 'booyah-bomb',
-                    },
-                    {
-                        name: locale('crab-tank'),
-                        value: 'crab-tank',
-                    },
-                    {
-                        name: locale('ink-storm'),
-                        value: 'ink-storm',
-                    },
-                    {
-                        name: locale('ink-vac'),
-                        value: 'ink-vac',
-                    },
-                    {
-                        name: locale('inkjet'),
-                        value: 'inkjet',
-                    },
-                    {
-                        name: locale('killer-wail-5.1'),
-                        value: 'killer-wail-5.1',
-                    },
-                    {
-                        name: locale('kraken-royale'),
-                        value: 'kraken-royale',
-                    },
-                    {
-                        name: locale('reefslider'),
-                        value: 'reefslider',
-                    },
-                    {
-                        name: locale('super-chump'),
-                        value: 'super-chump',
-                    },
-                    {
-                        name: locale('tacticooler'),
-                        value: 'tacticooler',
-                    },
-                    {
-                        name: locale('tenta-missiles'),
-                        value: 'tenta-missiles',
-                    },
-                    {
-                        name: locale('triple-inkstrike'),
-                        value: 'triple-inkstrike',
-                    },
-                    {
-                        name: locale('trizooka'),
-                        value: 'trizooka',
-                    },
-                    {
-                        name: locale('ultra-stamp'),
-                        value: 'ultra-stamp',
-                    },
-                    {
-                        name: locale('wave-breaker'),
-                        value: 'wave-breaker',
-                    },
-                    {
-                        name: locale('zipcaster'),
-                        value: 'zipcaster',
-                    },
+                    { name: locale('big-bubbler'), value: 'big-bubbler' },
+                    { name: locale('booyah-bomb'), value: 'booyah-bomb' },
+                    { name: locale('crab-tank'), value: 'crab-tank' },
+                    { name: locale('ink-storm'), value: 'ink-storm' },
+                    { name: locale('ink-vac'), value: 'ink-vac' },
+                    { name: locale('inkjet'), value: 'inkjet' },
+                    { name: locale('killer-wail-5.1'), value: 'killer-wail-5.1' },
+                    { name: locale('kraken-royale'), value: 'kraken-royale' },
+                    { name: locale('reefslider'), value: 'reefslider' },
+                    { name: locale('super-chump'), value: 'super-chump' },
+                    { name: locale('tacticooler'), value: 'tacticooler' },
+                    { name: locale('tenta-missiles'), value: 'tenta-missiles' },
+                    { name: locale('triple-inkstrike'), value: 'triple-inkstrike' },
+                    { name: locale('trizooka'), value: 'trizooka' },
+                    { name: locale('ultra-stamp'), value: 'ultra-stamp' },
+                    { name: locale('wave-breaker'), value: 'wave-breaker' },
+                    { name: locale('zipcaster'), value: 'zipcaster' },
                 )
         ),
 
     execute: async (interaction) => {
-        const type = interaction.options.get(locale('text-command-judge-name-1'))?.value as WeaponType | undefined;
+        const main = interaction.options.get(locale('text-command-judge-name-1'))?.value as WeaponType | undefined;
         const sub = interaction.options.get(locale('text-command-judge-name-2'))?.value as SubWeapon | undefined;
         const special = interaction.options.get(locale('text-command-judge-name-3'))?.value as SpecialWeapon | undefined;
 
         const weapon = judge(
-            type
-                ? [type]
+            main
+                ? [main]
                 : [],
             sub
                 ? [sub]
@@ -259,3 +134,62 @@ export const JudgeCommand: Command = {
         }
     },
 };
+
+const WEAPONS = [
+    ...SHOOTERS,
+    ...SPLATLINGS,
+    ...CHARGERS,
+    ...ROLLERS,
+    ...BLASTERS,
+    ...SLOSHERS,
+    ...DUALIES,
+    ...BRUSHES,
+    ...STRINGERS,
+    ...BRELLAS,
+    ...SPLATANAS,
+];
+
+function judge(typeFilters: WeaponType[], subFilters: SubWeapon[], specialFilters: SpecialWeapon[]): Weapon | undefined {
+    const weapons = WEAPONS
+        .filter((weapon) => {
+            if (typeFilters.length === 0) {
+                return true;
+            }
+
+            for (const filter of typeFilters) {
+                if (filter === weapon.type) {
+                    return true;
+                }
+            }
+
+            return false;
+        })
+        .filter((weapon) => {
+            if (subFilters.length === 0) {
+                return true;
+            }
+
+            for (const filter of subFilters) {
+                if (filter === weapon.sub) {
+                    return true;
+                }
+            }
+
+            return false;
+        })
+        .filter((weapon) => {
+            if (specialFilters.length === 0) {
+                return true;
+            }
+
+            for (const filter of specialFilters) {
+                if (filter === weapon.special) {
+                    return true;
+                }
+            }
+
+            return false;
+        });
+
+    return getRandomElem(weapons);
+}
